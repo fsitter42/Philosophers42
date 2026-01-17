8db3f526433f0ed270dc64af6c983c79576cd524
fsitter42 <fsitter@42vienna.com>
2026-01-17 12:26:48 +0100

    notepme

--- philo/f_input.c
+++ philo/f_input.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 11:56:29 by fsitter           #+#    #+#             */
-/*   Updated: 2026/01/17 12:20:33 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/01/17 12:26:31 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,14 @@ int	f_manage_input(int ac, char **av, t_in *input)
 	input->tte = f_valid_ulong(av[3]);
 	input->tts = f_valid_ulong(av[4]);
 	if (ac == 6)
+    {
 		input->notepme = f_valid_ulong(av[5]);
-	if (input->nop < 1 || input->ttd < 1 || input->tte < 1 || input->tts < 1
-		|| input->notepme < 1)
+        if (input->notepme < 1)
+            return (-1);
+    }
+    else
+        input->notepme = 0;
+	if (input->nop < 1 || input->ttd < 1 || input->tte < 1 || input->tts < 1)
 		return (-1);
 	return (0);
 }
