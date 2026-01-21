881f4a92e9ff7cad254fdacb906e2b4f295077fa
Your Name <you@example.com>
2026-01-21 11:42:06 +0100

    mut

--- sandbox/mutex.c
+++ sandbox/mutex.c
@@ -31,7 +31,6 @@ void free_t(t *data)
 void free_on_error(t *data, int exitcode)
 {
     free_t(data);
-    //free(data);
     exit(exitcode);
 }
 
@@ -56,17 +55,14 @@ void ex(void)
     pthread_t t1;
     pthread_t t2;
 
-    printf("%i\n", data->num);
-
     if (pthread_create(&t1, NULL, r1, data))
         free_on_error(data, 2);
     if (pthread_create(&t2, NULL, r1, data))
         free_on_error(data, 2);
 
-    printf("%i\n", data->num);
-    if (pthread_join(t1, NULL));
+    if (pthread_join(t1, NULL))
         free_on_error(data, 3);
-    if (pthread_join(t2, NULL));
+    if (pthread_join(t2, NULL))
         free_on_error(data, 3);
     
     printf("%i\n", data->num);
